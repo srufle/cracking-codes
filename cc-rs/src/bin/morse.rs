66d32f49177864f3f7ded5efc0cd9da328a83440
Stephen Rufle <srufle@gmail.com>
2020-06-02 07:00:19 -0700

    Adding rust based files

---
+++ cc-rs/src/bin/morse.rs
@@ -0,0 +1,64 @@
+use std::collections::HashMap;
+use std::io::{self, BufRead, Read};
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    let mut encoding_lookup = HashMap::new();
+
+    encoding_lookup.insert(String::from("A"), "*-");
+    encoding_lookup.insert(String::from("B"), "-***");
+    encoding_lookup.insert(String::from("C"), "-*-*");
+    encoding_lookup.insert(String::from("D"), "-**");
+    encoding_lookup.insert(String::from("E"), "*");
+    encoding_lookup.insert(String::from("F"), "**-*");
+    encoding_lookup.insert(String::from("G"), "--*");
+    encoding_lookup.insert(String::from("H"), "****");
+    encoding_lookup.insert(String::from("I"), "**");
+    encoding_lookup.insert(String::from("J"), "*---");
+    encoding_lookup.insert(String::from("K"), "-*-");
+    encoding_lookup.insert(String::from("L"), "*-**");
+    encoding_lookup.insert(String::from("M"), "--");
+    encoding_lookup.insert(String::from("N"), "-*");
+    encoding_lookup.insert(String::from("O"), "---");
+    encoding_lookup.insert(String::from("P"), "*--*");
+    encoding_lookup.insert(String::from("Q"), "--*-");
+    encoding_lookup.insert(String::from("R"), "*-*");
+    encoding_lookup.insert(String::from("S"), "***");
+    encoding_lookup.insert(String::from("T"), "-");
+    encoding_lookup.insert(String::from("U"), "**-");
+    encoding_lookup.insert(String::from("V"), "***-");
+    encoding_lookup.insert(String::from("W"), "*--");
+    encoding_lookup.insert(String::from("X"), "-**-");
+    encoding_lookup.insert(String::from("Y"), "-*---");
+    encoding_lookup.insert(String::from("Z"), "--**");
+    encoding_lookup.insert(String::from("1"), "*----");
+    encoding_lookup.insert(String::from("2"), "**---");
+    encoding_lookup.insert(String::from("3"), "***--");
+    encoding_lookup.insert(String::from("4"), "****-");
+    encoding_lookup.insert(String::from("5"), "*****");
+    encoding_lookup.insert(String::from("6"), "-****");
+    encoding_lookup.insert(String::from("7"), "--***");
+    encoding_lookup.insert(String::from("8"), "---**");
+    encoding_lookup.insert(String::from("9"), "----*");
+    encoding_lookup.insert(String::from("0"), "-----");
+
+    for (key, value) in &encoding_lookup {
+        println!("{}: {}", key, value);
+    }
+
+    // handle.read_to_string(&mut buffer)?;
+    handle.read_line(&mut buffer).unwrap();
+    println!("{}", buffer);
+    for c in buffer.chars() {
+        let upper_char = c.to_ascii_uppercase();
+        let upper_key = upper_char.to_string();
+        match encoding_lookup.get(&upper_key) {
+            Some(&value) => println!("{}", value),
+            _ => println!("Key not found '{}'", upper_key),
+        }
+    }
+
+    Ok(())
+}
