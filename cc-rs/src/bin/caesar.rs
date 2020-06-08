a60786a6c56985a91e8cfba4a8bf10fb93769ec2
Stephen Rufle <srufle@gmail.com>
2020-06-08 16:00:05 -0700

    Modified caesar to have real implementation

--- cc-rs/src/bin/caesar.rs
+++ cc-rs/src/bin/caesar.rs
@@ -1,64 +1,74 @@
 use std::collections::HashMap;
-use std::io::{self, BufRead, Read};
+use std::env;
+use std::io::{self, BufRead};
+
+static NA: &str = "NA";
 
 fn main() -> io::Result<()> {
-    // let mut buffer = String::new();
-    // let stdin = io::stdin();
-    // let mut handle = stdin.lock();
-    // let mut encoding_lookup = HashMap::new();
-
-    // encoding_lookup.insert(String::from("A"), "*-");
-    // encoding_lookup.insert(String::from("B"), "-***");
-    // encoding_lookup.insert(String::from("C"), "-*-*");
-    // encoding_lookup.insert(String::from("D"), "-**");
-    // encoding_lookup.insert(String::from("E"), "*");
-    // encoding_lookup.insert(String::from("F"), "**-*");
-    // encoding_lookup.insert(String::from("G"), "--*");
-    // encoding_lookup.insert(String::from("H"), "****");
-    // encoding_lookup.insert(String::from("I"), "**");
-    // encoding_lookup.insert(String::from("J"), "*---");
-    // encoding_lookup.insert(String::from("K"), "-*-");
-    // encoding_lookup.insert(String::from("L"), "*-**");
-    // encoding_lookup.insert(String::from("M"), "--");
-    // encoding_lookup.insert(String::from("N"), "-*");
-    // encoding_lookup.insert(String::from("O"), "---");
-    // encoding_lookup.insert(String::from("P"), "*--*");
-    // encoding_lookup.insert(String::from("Q"), "--*-");
-    // encoding_lookup.insert(String::from("R"), "*-*");
-    // encoding_lookup.insert(String::from("S"), "***");
-    // encoding_lookup.insert(String::from("T"), "-");
-    // encoding_lookup.insert(String::from("U"), "**-");
-    // encoding_lookup.insert(String::from("V"), "***-");
-    // encoding_lookup.insert(String::from("W"), "*--");
-    // encoding_lookup.insert(String::from("X"), "-**-");
-    // encoding_lookup.insert(String::from("Y"), "-*---");
-    // encoding_lookup.insert(String::from("Z"), "--**");
-    // encoding_lookup.insert(String::from("1"), "*----");
-    // encoding_lookup.insert(String::from("2"), "**---");
-    // encoding_lookup.insert(String::from("3"), "***--");
-    // encoding_lookup.insert(String::from("4"), "****-");
-    // encoding_lookup.insert(String::from("5"), "*****");
-    // encoding_lookup.insert(String::from("6"), "-****");
-    // encoding_lookup.insert(String::from("7"), "--***");
-    // encoding_lookup.insert(String::from("8"), "---**");
-    // encoding_lookup.insert(String::from("9"), "----*");
-    // encoding_lookup.insert(String::from("0"), "-----");
-
-    // for (key, value) in &encoding_lookup {
-    //     println!("{}: {}", key, value);
-    // }
+    let debug = env::var("DEBUG").is_ok();
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    let source_text = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let mut lookup = create_lookup(source_text, 3);
 
     // // handle.read_to_string(&mut buffer)?;
-    // handle.read_line(&mut buffer).unwrap();
-    // println!("{}", buffer);
-    // for c in buffer.chars() {
-    //     let upper_char = c.to_ascii_uppercase();
-    //     let upper_key = upper_char.to_string();
-    //     match encoding_lookup.get(&upper_key) {
-    //         Some(&value) => println!("{}", value),
-    //         _ => println!("Key not found '{}'", upper_key),
-    //     }
-    // }
-    println!("caesar");
+    handle.read_line(&mut buffer).unwrap();
+    println!("{}", buffer);
+    let mut result: Vec<String> = vec![];
+    for c in buffer.chars() {
+        let encoded = encode(c, &lookup);
+        result.push(encoded.1);
+    }
+    println!("{}", result.join(""));
     Ok(())
+}
+
+fn create_lookup(source: &str, offset: usize) -> HashMap<String, String> {
+    let mut lookup: HashMap<String, String> = HashMap::new();
+    let mut letters: Vec<_> = source.split("").collect();
+    letters.remove(0);
+    letters.remove(letters.len() - 1);
+    // println!("{:?}", letters);
+    let letters_len = letters.len();
+    let start = 0;
+
+    for index in start..letters_len {
+        // let index: usize = idx as usize;
+        let new_index: usize = (index + offset) % letters_len;
+        let key = letters.get(index);
+        let value = letters.get(new_index);
+        match key {
+            Some(k) => match value {
+                Some(v) => {
+                    lookup.insert(
+                        k.to_ascii_uppercase().to_string(),
+                        v.to_ascii_uppercase().to_string(),
+                    );
+                }
+                _ => {
+                    lookup.insert(NA.to_string(), NA.to_string());
+                }
+            },
+            _ => {
+                lookup.insert(NA.to_string(), NA.to_string());
+            }
+        }
+
+        //     // println!("{}", new_index);
+    }
+    lookup.insert("offset".to_string(), offset.to_string());
+    lookup
+}
+
+fn encode(letter: char, lookup: &HashMap<String, String>) -> (String, String) {
+    let upper_char = letter.to_ascii_uppercase();
+    match lookup.get(&(upper_char.to_string())) {
+        Some(value) => {
+            return (upper_char.to_string(), value.to_string());
+        }
+        _ => {
+            return (upper_char.to_string(), upper_char.to_string());
+        }
+    }
 }
