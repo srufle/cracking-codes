28708b7b16b31daeddf550fb33f3dfe3e3ede9ff
Stephen Rufle <srufle@gmail.com>
2020-06-09 06:57:57 -0700

    Modified caesar.py - Changed to use command line args - Changed approach to [en|de]coding

--- cc-py/caesar.py
+++ cc-py/caesar.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python3
 
 import sys
+import argparse
 
-source_text = "abcdefghijklmnopqrstuvwxyz1234567890"
+parser = argparse.ArgumentParser(description="Example")
+parser.add_argument("-m", "--message", type=str)
+parser.add_argument("-e", "--mode", type=str, default="enc")
+parser.add_argument("-o", "--offset", type=int, default=3)
 
+args = parser.parse_args()
 
-def create_lookup(source, offset=3):
-    encoding_lookup = {}
-    encoding_lookup['offset'] = offset
-    source_len = len(source)
-    for index in range(len(source)):
-        new_index = (index+offset) % source_len
-        print(f"{source[index]} = {source[new_index]}")
-        encoding_lookup[source[index].upper()] = source[new_index].upper()
-    return encoding_lookup
+SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!?."
 
-
-def encode(char):
-    upper_char = char.upper()
-    if upper_char in encoding_lookup.keys():
-        encoded_char = encoding_lookup[upper_char]
-    else:
-        encoded_char = char
-
-    return (upper_char, encoded_char)
-
-
+message = args.message
+symbols_len = len(SYMBOLS)
+offset = args.offset
+mode = args.mode
+if message == None:
+    for line in sys.stdin:
+        message = line.rstrip()
 result = []
-encoding_lookup = create_lookup(source_text)
-for line in sys.stdin:
-    chars = line.rstrip()
-    for char in chars:
-        encoded = encode(char)
-        print(f"{encoded[0]} = {encoded[1]}")
-        result += [encoded[1]]
+for symbol in message:
+    if symbol in SYMBOLS:
+        symbol_index = SYMBOLS.find(symbol)
+        if mode == "enc":
+            translated_index = symbol_index + offset
+        elif mode == "dec":
+            translated_index = symbol_index - offset
 
+        if translated_index >= symbols_len:
+            translated_index = symbol_index - symbols_len
+        elif translated_index < 0:
+            translated_index = symbol_index + symbols_len
+        result += SYMBOLS[translated_index]
+    else:
+        result += symbol
 
 print("Message:")
-print(f"{encoding_lookup['offset']}{''.join(result)}")
+print(f"{offset}:{''.join(result)}")
