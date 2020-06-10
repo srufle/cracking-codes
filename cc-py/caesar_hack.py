0733871c89a3f3b64126c688d879d526afd84e59
Stephen Rufle <srufle@gmail.com>
2020-06-10 05:46:25 -0700

    Adding brute force caesar_hack.py

---
+++ cc-py/caesar_hack.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="Example")
+parser.add_argument("-m", "--message", type=str)
+
+args = parser.parse_args()
+
+SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!?."
+
+message = args.message
+symbols_len = len(SYMBOLS)
+if message == None:
+    for line in sys.stdin:
+        message = line.rstrip()
+for key in range(symbols_len):
+    result = []
+    for symbol in message:
+        if symbol in SYMBOLS:
+            symbol_index = SYMBOLS.find(symbol)
+            translated_index = symbol_index - key
+
+            if translated_index < 0:
+                translated_index = translated_index + symbols_len
+
+            result += SYMBOLS[translated_index]
+        else:
+            result += symbol
+    print("Message:")
+    print(f"{key}:{''.join(result)}")
