5d7a20f36edc9d6e14d93825bf2f611290c93e0b
Stephen Rufle <srufle@gmail.com>
2020-06-28 20:50:54 -0700

    Adding vigenere_hack module and test

--- cc-py/test_vigenere_hack.py
+++ cc-py/test_vigenere_hack.py
@@ -4,6 +4,7 @@ import logging as log
 import detect_english as de
 import vigenere_hack as vh
 import freq_analysis as fa
+import pytest
 
 log.basicConfig(level=log.INFO)
 test_quote = """Alan Mathison Turing was a British mathematician, logician, cryptanalyst, and computer
@@ -22,7 +23,8 @@ test_quote = """Alan Mathison Turing was a British mathematician, logician, cryp
             such as the Belousov-Zhabotinsky reaction, which were first observed in the 1960s. Turing's
             homosexuality resulted in a criminal prosecution in 1952, when homosexual acts were still
             illegal in the United Kingdom. He accepted treatment with female hormones (chemical castration)
-            as an alternative to prison. Turing died in 1954, just over two weeks before his 42nd birthday,
+            as an alternative to prison. Turing
+             died in 1954, just over two weeks before his 42nd birthday,
             from cyanide poisoning. An inquest determined that his death was suicide; his mother and some
             others believed his death was accidental. On 10 September 2009, following an Internet campaign,
             British Prime Minister Gordon Brown made an official public apology on behalf of the British
@@ -30,7 +32,7 @@ test_quote = """Alan Mathison Turing was a British mathematician, logician, cryp
             before the House of Lords which would grant Turing a statutory pardon if enacted."""
 
 
-def test_vigenere_hack():
+def test_vigenere_hack_dict():
     words_file = de.get_words_file_path("content/dictionary.txt")
     data = de.load_data(words_file)
 
@@ -43,3 +45,29 @@ def test_vigenere_hack():
     assert expected_key in hacked_messages.keys()
     assert expected_message in hacked_messages.values()
 
+
+def test_vigenere_hack_kasiski():
+    words_file = de.get_words_file_path("content/dictionary.txt")
+    data = de.load_data(words_file)
+
+    message = """Adiz Avtzqeci Tmzubb wsa m Pmilqev halpqavtakuoi, lgouqdaf, kdmktsvmztsl, izr xoexghzr kkusitaaf. Vz wsa twbhdg ubalmmzhdad qz hce vmhsgohuqbo ox kaakulmd gxiwvos, krgdurdny i rcmmstugvtawz ca tzm ocicwxfg jf "stscmilpy" oid "uwydptsbuci" wabt hce Lcdwig eiovdnw. Bgfdny qe kddwtk qjnkqpsmev ba pz tzm roohwz at xoexghzr kkusicw izr vrlqrwxist uboedtuuznum. Pimifo Icmlv Emf DI, Lcdwig owdyzd xwd hce Ywhsmnemzh Xovm mby Cqxtsm Supacg (GUKE) oo Bdmfqclwg Bomk, Tzuhvif'a ocyetzqofifo ositjm. Rcm a lqys ce oie vzav wr Vpt 8, lpq gzclqab mekxabnittq tjr Ymdavn fihog cjgbhvnstkgds. Zm psqikmp o iuejqf jf lmoviiicqg aoj jdsvkavs Uzreiz qdpzmdg, dnutgrdny bts helpar jf lpq pjmtm, mb zlwkffjmwktoiiuix avczqzs ohsb ocplv nuby swbfwigk naf ohw Mzwbms umqcifm. Mtoej bts raj pq kjrcmp oo tzm Zooigvmz Khqauqvl Dincmalwdm, rhwzq vz cjmmhzd gvq ca tzm rwmsl lqgdgfa rcm a kbafzd-hzaumae kaakulmd, hce SKQ. Wi 1948 Tmzubb jgqzsy Msf Zsrmsv'e Qjmhcfwig Dincmalwdm vt Eizqcekbqf Pnadqfnilg, ivzrw pq onsaafsy if bts yenmxckmwvf ca tzm Yoiczmehzr uwydptwze oid tmoohe avfsmekbqr dn eifvzmsbuqvl tqazjgq. Pq kmolm m dvpwz ab ohw ktshiuix pvsaa at hojxtcbefmewn, afl bfzdakfsy okkuzgalqzu xhwuuqvl jmmqoigve gpcz ie hce Tmxcpsgd-Lvvbgbubnkq zqoxtawz, kciup isme xqdgo otaqfqev qz hce 1960k. Bgfdny'a tchokmjivlabk fzsmtfsy if i ofdmavmz krgaqqptawz wi 1952, wzmz vjmgaqlpad iohn wwzq goidt uzgeyix wi tzm Gbdtwl Wwigvwy. Vz aukqdoev bdsvtemzh rilp rshadm tcmmgvqg (xhwuuqvl uiehmalqab) vs sv mzoejvmhdvw ba dmikwz. Hpravs rdev qz 1954, xpsl whsm tow iszkk jqtjrw pug 42id tqdhcdsg, rfjm ugmbddw xawnofqzu. Vn avcizsl lqhzreqzsy tzif vds vmmhc wsa eidcalq; vds ewfvzr svp gjmw wfvzrk jqzdenmp vds vmmhc wsa mqxivmzhvl. Gv 10 Esktwunsm 2009, fgtxcrifo mb Dnlmdbzt uiydviyv, Nfdtaat Dmiem Ywiikbqf Bojlab Wrgez avdw iz cafakuog pmjxwx ahwxcby gv nscadn at ohw Jdwoikp scqejvysit xwd "hce sxboglavs kvy zm ion tjmmhzd." Sa at Haq 2012 i bfdvsbq azmtmd'g widt ion bwnafz tzm Tcpsw wr Zjrva ivdcz eaigd yzmbo Tmzubb a kbmhptgzk dvrvwz wa efiohzd."""
+    _is_done, hacked_messages = vh.hack_vigenere_kasiski(message, data)
+    # for word in hacked_messages.keys():
+    #     print(f"{word}: {hacked_messages[word]}")
+    expected_key = "ASIMOV"
+    expected_message = """Alan Mathison Turing was a British mathematician, logician, cryptanalyst, and computer scientist. He was highly influential in the development of computer science, providing a formalisation of the concepts of "algorithm" and "computation" with the Turing machine. Turing is widely considered to be the father of computer science and artificial intelligence. During World War II, Turing worked for the Government Code and Cypher School (GCCS) at Bletchley Park, Britain's codebreaking centre. For a time he was head of Hut 8, the section responsible for German naval cryptanalysis. He devised a number of techniques for breaking German ciphers, including the method of the bombe, an electromechanical machine that could find settings for the Enigma machine. After the war he worked at the National Physical Laboratory, where he created one of the first designs for a stored-program computer, the ACE. In 1948 Turing joined Max Newman's Computing Laboratory at Manchester University, where he assisted in the development of the Manchester computers and became interested in mathematical biology. He wrote a paper on the chemical basis of morphogenesis, and predicted oscillating chemical reactions such as the Belousov-Zhabotinsky reaction, which were first observed in the 1960s. Turing's homosexuality resulted in a criminal prosecution in 1952, when homosexual acts were still illegal in the United Kingdom. He accepted treatment with female hormones (chemical castration) as an alternative to prison. Turing died in 1954, just over two weeks before his 42nd birthday, from cyanide poisoning. An inquest determined that his death was suicide; his mother and some others believed his death was accidental. On 10 September 2009, following an Internet campaign, British Prime Minister Gordon Brown made an official public apology on behalf of the British government for "the appalling way he was treated." As of May 2012 a private member's bill was before the House of Lords which would grant Turing a statutory pardon if enacted."""
+    assert expected_key in hacked_messages.keys()
+    assert expected_message in hacked_messages.values()
+
+
+def test_vigenere_hack_get_useful_factors():
+    factors = vh.get_useful_factors(144)
+    expected_factors = [2, 3, 4, 6, 8, 9, 12, 16]
+    assert expected_factors == factors
+
+
+def test_vigenere_hack_get_nth_subkeys_letters():
+    assert vh.get_nth_subkeys_letters(1, 3, "ABCABCABC") == "AAA"
+    assert vh.get_nth_subkeys_letters(2, 3, "ABCABCABC") == "BBB"
+    assert vh.get_nth_subkeys_letters(3, 3, "ABCABCABC") == "CCC"
+    assert vh.get_nth_subkeys_letters(1, 5, "ABCDEFGHI") == "AF"
--- cc-py/vigenere_hack.py
+++ cc-py/vigenere_hack.py
@@ -3,7 +3,9 @@ import argparse
 import datetime
 import os
 import logging as log
-import random as r
+
+import re
+import itertools
 import sys
 import time
 import vigenere as v
@@ -11,14 +13,23 @@ import freq_analysis as fa
 
 import detect_english as de
 
+MAX_KEY_LENGTH = 16
+NUM_MOST_FREQ_LETTERS = 4
+SILENT_MODE = True
+NONLETTERS_PATTERN = re.compile("[^A-Z]")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Vigenere Cipher Hack")
     parser.add_argument("-m", "--message", type=str)
+    parser.add_argument(
+        "-t", "--type", type=str, choices=["dict", "kski"], default="dict"
+    )
     # parser.add_argument("-f", "--file", type=str)
 
     args = parser.parse_args()
     message = args.message
+    hack_type = args.type
     file_to_use = None  # args.file
 
     words_file = de.get_words_file_path("content/dictionary.txt")
@@ -38,14 +49,234 @@ def main():
             message = fo.read()
 
     start_time = time.time()
-    hacked_messages = hack_vigenere_dict(message, data)
-    if len(hacked_messages) == 0:
-        total_time = round(time.time() - start_time, 2)
-        print(f"Completed in: {datetime.timedelta(seconds=total_time)}")
-        for word in hacked_messages.keys():
-            print(f"{word}: {hacked_messages[word]}")
+    if hack_type == "dict":
+        hacked_messages = hack_vigenere_dict(message, data)
+        if len(hacked_messages) == 0:
+            total_time = round(time.time() - start_time, 2)
+            print(f"Completed in: {datetime.timedelta(seconds=total_time)}")
+            for word in hacked_messages.keys():
+                print(f"{word}: {hacked_messages[word]}")
+        else:
+            print("Failed to decrypt")
+    elif hack_type == "kski":
+        _is_done, hacked_messages = hack_vigenere_kasiski(message, data)
+        if len(hacked_messages) == 0:
+            total_time = round(time.time() - start_time, 2)
+            print(f"Completed in: {datetime.timedelta(seconds=total_time)}")
+            for word in hacked_messages.keys():
+                print(f"{word}: {hacked_messages[word]}")
+        else:
+            print("Failed to decrypt")
     else:
-        print("Failed to decrypt")
+        print("Hack type unknown")
+
+
+def find_repeat_sequences_spacings(message):
+    # Goes through the message and finds any 3- to 5-letter sequences
+    # that are repeated. Returns a dict with the keys of the sequence and
+    # values of a list of spacings (num of letters between the repeats).
+
+    message = NONLETTERS_PATTERN.sub("", message.upper())
+    message_len = len(message)
+
+    seq_spacings = {}
+    for seq_len in range(3, 6):
+        for seq_start in range(message_len - seq_len):
+            seq = message[seq_start : seq_start + seq_len]
+
+            for i in range(seq_start + seq_len, message_len - seq_len):
+                if message[i : i + seq_len] == seq:
+                    if seq not in seq_spacings:
+                        seq_spacings[seq] = []
+
+                    seq_spacings[seq].append(i - seq_start)
+
+    return seq_spacings
+
+
+def get_useful_factors(num):
+    # Returns a list of useful factors of num. By "useful" we mean factors
+    # less than MAX_KEY_LENGTH + 1 and not 1. For example,
+    # getUsefulFactors(144) returns [2, 3, 4, 6, 8, 9, 12, 16].
+
+    if num < 2:
+        return []
+
+    factors = []
+    for i in range(2, MAX_KEY_LENGTH + 1):
+        if num % i == 0:
+            factors.append(i)
+            other_factor = int(num / i)
+            if other_factor < MAX_KEY_LENGTH + 1 and other_factor != 1:
+                factors.append(other_factor)
+
+    return list(set(factors))
+
+
+def get_item_at_index_one(x):
+    return x[1]
+
+
+def get_most_common_factors(seq_factors):
+    factor_counts = {}
+
+    for seq in seq_factors:
+        factor_list = seq_factors[seq]
+        for factor in factor_list:
+            if factor not in factor_counts:
+                factor_counts[factor] = 0
+
+            factor_counts[factor] += 1
+
+    factors_by_count = []
+
+    for factor in factor_counts:
+        if factor <= MAX_KEY_LENGTH:
+            factors_by_count.append((factor, factor_counts[factor]))
+
+    factors_by_count.sort(key=get_item_at_index_one, reverse=True)
+
+    return factors_by_count
+
+
+def kasiski_examination(message):
+    repeated_seq_spacings = find_repeat_sequences_spacings(message)
+
+    seq_factors = {}
+    for seq in repeated_seq_spacings:
+        seq_factors[seq] = []
+        for spacing in repeated_seq_spacings[seq]:
+            seq_factors[seq].extend(get_useful_factors(spacing))
+
+    factors_by_count = get_most_common_factors(seq_factors)
+
+    all_likely_lengths = []
+    for two_int_tuple in factors_by_count:
+        all_likely_lengths.append(two_int_tuple[0])
+
+    return all_likely_lengths
+
+
+def get_nth_subkeys_letters(nth, key_length, message):
+    # Returns every nth letter for each key_length set of letters in text.
+    # E.g. getNthSubkeysLetters(1, 3, 'ABCABCABC') returns 'AAA'
+    #      getNthSubkeysLetters(2, 3, 'ABCABCABC') returns 'BBB'
+    #      getNthSubkeysLetters(3, 3, 'ABCABCABC') returns 'CCC'
+    #      getNthSubkeysLetters(1, 5, 'ABCDEFGHI') returns 'AF'
+
+    message = NONLETTERS_PATTERN.sub("", message)
+
+    i = nth - 1
+    letters = []
+    while i < len(message):
+        letters.append(message[i])
+        i += key_length
+
+    return "".join(letters)
+
+
+def attempt_hack_with_key_length(message, likely_key_length, data):
+    possible_decrypted_texts = {}
+    message_upper = message.upper()
+
+    all_freq_scores = []
+    for nth in range(1, likely_key_length + 1):
+        nth_letters = get_nth_subkeys_letters(nth, likely_key_length, message_upper)
+
+        freq_scores = []
+        LETTERS = data["LETTERS"]
+        for possible_key in LETTERS:
+            decrypted_text = v.decrypt_message(possible_key, nth_letters, data)
+            key_and_freq_match_tuple = (
+                possible_key,
+                fa.english_freq_match(decrypted_text, data),
+            )
+            freq_scores.append(key_and_freq_match_tuple)
+
+        freq_scores.sort(key=get_item_at_index_one, reverse=True)
+
+        all_freq_scores.append(freq_scores[:NUM_MOST_FREQ_LETTERS])
+
+    if not SILENT_MODE:
+        for i in range(len(all_freq_scores)):
+            # Use i + 1 so the first letter is not called the "0th" letter:
+            print(f"Possible letters for letter {i+1} of the key: ", end="")
+            for freq_score in all_freq_scores[i]:
+                print(f"{freq_score[0]} ", end="")
+            print()  # Print a newline.
+
+    for indexes in itertools.product(
+        range(NUM_MOST_FREQ_LETTERS), repeat=likely_key_length
+    ):
+        possible_key = ""
+        for i in range(likely_key_length):
+            log.debug(f"i={i}, indexes[{i}]={indexes[i]}, {all_freq_scores}")
+            possible_key += all_freq_scores[i][indexes[i]][0]
+
+        if not SILENT_MODE:
+            print(f"Attempting with key: ({possible_key})")
+        decrypted_text = v.decrypt_message(possible_key, message_upper, data)
+
+        if de.is_english(decrypted_text, data):
+            orig_case = []
+            for i in range(len(message)):
+                if message[i].isupper():
+                    orig_case.append(decrypted_text[i].upper())
+                else:
+                    orig_case.append(decrypted_text[i].lower())
+
+            decrypted_text = "".join(orig_case)
+            possible_decrypted_texts[possible_key] = decrypted_text
+            print("Possible encryption hack with key %s:" % (possible_key))
+            print(decrypted_text[:200])  # Only show first 200 characters.
+            print()
+            print("Enter D if done, anything else to continue hacking:")
+            response = input("> ")
+            if response.strip().upper().startswith("D"):
+                return (True, possible_decrypted_texts)
+    return (False, {})
+
+
+def hack_vigenere_kasiski(message, data):
+    is_done = False
+    possible_decrypted_texts = {}
+    likely_key_lengths = kasiski_examination(message)
+    if not SILENT_MODE:
+        key_length_str = ""
+        for key_length in likely_key_lengths:
+            key_length_str += "%s " % (key_length)
+        print(
+            f"Kasiski examination results say the most likely key lengths are: {key_length_str}"
+        )
+
+    for likely_key_length in likely_key_lengths:
+        if not SILENT_MODE:
+            print(
+                f"Attempting hack with key length {likely_key_length} ({NUM_MOST_FREQ_LETTERS ** likely_key_length} possible keys)..."
+            )
+        is_done, possible_decrypted_texts = attempt_hack_with_key_length(
+            message, likely_key_length, data
+        )
+        if not is_done and len(possible_decrypted_texts) == 0:
+            if not SILENT_MODE:
+                print(
+                    "Unable to hack message with likely key length(s). Brute-forcing key length..."
+                )
+            for key_length in range(1, MAX_KEY_LENGTH + 1):
+                if key_length not in likely_key_lengths:
+                    if not SILENT_MODE:
+                        print(
+                            f"Attempting hack with key length {key_length} ({NUM_MOST_FREQ_LETTERS ** key_length} possible keys)..."
+                        )
+                    is_done, possible_decrypted_texts = attempt_hack_with_key_length(
+                        message, key_length, data
+                    )
+                if is_done and len(possible_decrypted_texts) != 0:
+                    return (is_done, possible_decrypted_texts)
+        else:
+            return (is_done, possible_decrypted_texts)
+
+    return (is_done, possible_decrypted_texts)
 
 
 def hack_vigenere_dict(message, data):
